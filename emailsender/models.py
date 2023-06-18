bc1e432e5238fafeb5c6b2db849f0c5827d50f26
Sgrneane <sgrneaneprof@gmail.com>
2023-06-18 16:07:37 +0545

    message

--- emailsender/models.py
+++ emailsender/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Group(models.Model):
     name=models.CharField(max_length=60,unique=True)
+    description=models.TextField()
     emails=models.ManyToManyField('Email',related_name='groups')
     def __str__(self):
         return self.name
