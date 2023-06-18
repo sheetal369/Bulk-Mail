099a62d4955c2e11f19c4c125d935eca10121574
Sgrneane <sgrneaneprof@gmail.com>
2023-06-18 14:11:18 +0545

    Model Creation & registration

--- emailsender/models.py
+++ emailsender/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Group(models.Model):
+    name=models.CharField(max_length=60,unique=True)
+    emails=models.ManyToManyField('Email',related_name='groups')
+    def __str__(self):
+        return self.name
+    
+class Email(models.Model):
+    email_address= models.EmailField(unique=True,null=False,max_length=40)
+    def __str__(self):
+        return self.email_address
+    
+class Message(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
