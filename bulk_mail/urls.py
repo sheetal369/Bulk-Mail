0f767a2679b6e47be7772f67ba9cfd30a6d93b3d
Sheetal Sharma <sheetalsharma0719@gmail.com>
2023-06-19 13:53:20 +0545

    Views & Group Working state

--- bulk_mail/urls.py
+++ bulk_mail/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('view_contacts/', view_contacts, name="view_contacts"),
     path('view_groups/', view_groups, name="view_groups"),
     path('create_group/', create_group, name="create_group"),
-    path('view_group/', view_group, name="view_group"),
+    path('group/<int:id>/', group_detail, name='group'),
+    # path('view_group/<int>', view_group, name="view_group"),
 ]
