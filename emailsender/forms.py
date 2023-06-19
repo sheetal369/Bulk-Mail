4076d6822ca892ac38a5a4a0f09e28916a616e56
Sheetal Sharma <sheetalsharma0719@gmail.com>
2023-06-19 11:06:26 +0545

    Create & View contacts

--- emailsender/forms.py
+++ emailsender/forms.py
@@ -1,10 +1,16 @@
 from django import forms
-from .models import Group,Email
+from .models import Group, User
 
 
 class CreateGroup(forms.Form):
     name=forms.CharField(required=True,max_length=50)
-    emails=forms.ModelMultipleChoiceField(queryset=Email.objects.all())
+    emails=forms.ModelMultipleChoiceField(queryset=User.objects.values_list('email_address', flat=True))
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['full_name', 'email_address', 'phone_number']
+
 
 class GroupEmailForm(forms.Form):
     group = forms.ModelChoiceField(queryset=Group.objects.all())
